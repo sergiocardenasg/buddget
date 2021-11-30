549c3832ff03fbc7fe0060a47954bd67db80d447
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-30 08:55:38 -0600

    more router work

--- buddget_client/src/Components/Router.js
+++ buddget_client/src/Components/Router.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import Home from './Home';
+import BudgetsContainer from './BudgetsContainer';
+import About from './About';
 
 export default function Router() {
     return (
         <div>
-            
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/Budgets' component={BudgetsContainer} />
+                <Route exact path='/about' component={About} />
+            </Switch>
         </div>
     )
 }
