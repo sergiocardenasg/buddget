648536202caaa9f6d63861436596edcf3481f12b
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-30 08:27:52 -0600

    router start

---
+++ buddget_client/src/Components/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+export default function Router() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- buddget_client/src/index.js
+++ buddget_client/src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { rootReducer } from './Reducers/rootReducer';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const store = createStore(
   rootReducer, 
@@ -16,7 +17,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
