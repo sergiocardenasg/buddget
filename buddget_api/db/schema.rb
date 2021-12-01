e714df2542ebeb0e02f6501556a1bdb8491e9950
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-30 20:06:30 -0600

    revert schema

--- buddget_api/db/schema.rb
+++ buddget_api/db/schema.rb
@@ -10,21 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_205007) do
-
-  create_table "budgets", force: :cascade do |t|
-    t.string "name"
-    t.integer "fund_amount"
-    t.integer "expenses"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "expenses", force: :cascade do |t|
-    t.string "name"
-    t.integer "amount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
