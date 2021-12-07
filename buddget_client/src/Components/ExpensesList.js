88d6f253f4ff8ccd6a81ada23e12fbe906393a1a
sergiocardenasg <sergiocardenas@utexas.edu>
2021-12-06 20:07:00 -0600

    expenses rendering

--- buddget_client/src/Components/ExpensesList.js
+++ buddget_client/src/Components/ExpensesList.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const ExpensesList = ({ match, budgets }) => {
   let bdgt = budgets.find(budget => budget.id === parseInt(match.params.id))
-  const renderExpenses = (bdgt.expenses).map((expense) => (
+  const renderExpenses = () => (bdgt.expenses).map((expense) => (
     <ul>
         <li>
             {expense.name} - ${expense.amount}
@@ -11,7 +11,8 @@ const ExpensesList = ({ match, budgets }) => {
     </ul>
   ));
 
-  return <div>{renderExpenses} 
+  
+  return <div>{!!bdgt ? renderExpenses() : null} 
     <NavLink to={`/budgets/${match.params.id}/expenses/new`}> 
       Add Expense to Budget
     </NavLink>
