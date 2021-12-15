a6f793e9b2c3351b083096a0d202b99123d519c9
sergiocardenasg <sergiocardenas@utexas.edu>
2021-12-14 21:42:36 -0600

    about page

--- buddget_client/src/Components/About.js
+++ buddget_client/src/Components/About.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            this is an about page
+            <h3>More resources:</h3>
+                <ul>
+                    <li> <a href="https://pocketguard.com/"> PocketGuard, for a simplified budgeting snapshot </a></li>
+                    <li> <a href="https://mint.intuit.com/"> Mint, for budgeting and credit monitoring</a></li>
+                    <li> <a href="https://www.youneedabudget.com/"> YNAB and EveryDollar, for zero-based budgeting</a></li>
+                    <li> <a href="https://goodbudget.com/"> Goodbudget, for shared envelope-budgeting</a></li>
+                    <li> <a href="https://www.honeydue.com/">Honeydue, for budgeting with your partner</a></li>
+                    <li> <a href="https://www.personalcapital.com/">Personal Capital, for tracking wealth and spending</a></li>
+                </ul>
         </div>
     )
 }
