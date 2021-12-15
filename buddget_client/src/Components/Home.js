7557fdf651d937665322c0a88c1efae938c86ad6
sergiocardenasg <sergiocardenas@utexas.edu>
2021-12-14 21:29:20 -0600

    home page

--- buddget_client/src/Components/Home.js
+++ buddget_client/src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-//import ".//Buddget-logos_black.png"
+import buddget from "../Buddget-logos_black.png"
 
 export default function Home() {
     return (
         <div>
             <h2> Your budget is your buddy! </h2>
-            <img src="../Buddget-logos_black.png" alt="Buddget Logo"/>
+            <img src={buddget} alt="Buddget Logo" width="350"/>
             <h3> Wanna get your financial house in order? Try Buddget! Buddget makes it easy to track your expenses so you're not left wondering why your bank account is empty whenever you check it!</h3>
         </div>
     )
