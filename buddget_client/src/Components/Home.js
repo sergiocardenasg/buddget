d78e2e27512cf09979fb96d41db0365a30c3739c
sergiocardenasg <sergiocardenas@utexas.edu>
2021-12-29 14:38:31 -0700

    update class to className

--- buddget_client/src/Components/Home.js
+++ buddget_client/src/Components/Home.js
@@ -3,7 +3,7 @@ import buddget from "../Buddget-logos_black.png"
 
 export default function Home() {
     return (
-        <div class="w3-container w3-center">
+        <div className="w3-container w3-center">
             <h2> Your budget is your buddy! </h2>
             <img src={buddget} alt="Buddget Logo" width="350"/>
             <h3> Wanna get your financial house in order? Try Buddget! Buddget makes it easy to track your expenses so you're not left wondering why your bank account is empty whenever you check it!</h3>
